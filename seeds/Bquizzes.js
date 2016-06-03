f8e5475aeb6074c21b508c22b17a20275fb68f8a
Andy Gross <andogro@gmail.com>
2016-06-03 09:40:12 -0600

    index at beginning

--- seeds/Bquizzes.js
+++ seeds/Bquizzes.js
@@ -7,7 +7,6 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     knex('quizzes').insert(
         {
-          quiz_id: 5,
           user_id: 1,
           quiz_name: 'Quiz1',
           quiz_desc: 'The First Test',
@@ -26,9 +25,37 @@ exports.seed = function(knex, Promise) {
         }),
     knex('quizzes').insert(
         {
-          user_id: 3,
+          user_id: 2,
           quiz_name: 'Quiz3',
-          quiz_desc: 'The Tech Bubble',
+          quiz_desc: 'Javascript2',
+          quiz_tags: 'angular, ember',
+          created: '2016-03-16 20:07:28-06',
+          last_update: '2016-03-21 20:07:28-06'
+        }),
+    knex('quizzes').insert(
+        {
+          user_id: 2,
+          quiz_name: 'Quiz4',
+          quiz_desc: 'Javascript3',
+          quiz_tags: 'angular, ember',
+          created: '2016-03-16 20:07:28-06',
+          last_update: '2016-03-21 20:07:28-06'
+        }),
+    knex('quizzes').insert(
+        {
+          user_id: 2,
+          quiz_name: 'The Tech Bubble',
+          quiz_desc: 'Is the tech bubble about to burst?',
+          quiz_tags: 'angular, ember',
+          created: '2016-03-16 20:07:28-06',
+          last_update: '2016-03-21 20:07:28-06'
+        }),
+
+    knex('quizzes').insert(
+        {
+          user_id: 3,
+          quiz_name: 'Steve Jobs',
+          quiz_desc: 'A quiz about Steve Jobs',
           quiz_tags: 'javascript, Steve Jobs',
           created: '2016-04-22 20:07:28-06',
           last_update: '2016-05-02 20:07:28-06'
